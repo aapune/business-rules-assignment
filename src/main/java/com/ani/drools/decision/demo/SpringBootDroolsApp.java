2e48d22ad36345886d15a7dea2e4b7ef3bacaba4
Aniruddha Anikhindi <aa.pune@gmail.com>
2022-02-09 03:55:42 +0530

    JIRA:101 - Create business rules starter project with Spring boot

---
+++ src/main/java/com/ani/drools/decision/demo/SpringBootDroolsApp.java
@@ -0,0 +1,13 @@
+package com.ani.drools.decision.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDroolsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDroolsApp.class, args);
+	}
+
+}
