75495179ade3d53a317e609f6f1f465c3097da84
Aniruddha Anikhindi <aa.pune@gmail.com>
2022-02-09 04:01:58 +0530

    JIRA:101 - Start TDD with writing test cases in Restassured - Book testcase

---
+++ src/test/java/com/ani/drools/decision/demo/SpringBootDroolsAppTests.java
@@ -0,0 +1,47 @@
+package com.ani.drools.decision.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+@SpringBootTest(classes = SpringBootDroolsAppTests.class)
+
+
+/**
+ * 
+ * @author Ani
+ * Test suite for business rules assignment
+ * These are RESTassured based unit test cases reason for this implementation is its easy/fast to implement basic test cases
+ *
+ */
+public class SpringBootDroolsAppTests {
+	
+	
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testBookInput() {
+		RestAssured.baseURI = "http://localhost:9999";
+		RequestSpecification request = RestAssured.given();
+		JSONObject requestParams = new JSONObject();
+		request.contentType(ContentType.JSON);
+		requestParams.put("paymentType", "BOOK");
+		request.body(requestParams.toJSONString());
+		Response response = request.post("/get-output");
+		ResponseBody<?> body = response.getBody();
+		System.out.println(response.getStatusLine());
+		System.out.println(body.asString());
+		assertThat(response.getStatusCode()).isEqualTo(200);
+	}
+
+	
+
+}
